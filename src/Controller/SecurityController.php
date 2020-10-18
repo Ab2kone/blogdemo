16dc010140bb243bdc72e56abe6c4a0e07e24e78
Ab2kone <ab2kone@gmail.com>
2020-10-18 21:43:25 +0100

    Blogdemo with symfony 4

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Form\RegistrationType;
+use App\Entity\User;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
+class SecurityController extends AbstractController
+{
+    
+    /**
+     * @Route("/inscription", name="security_registration")
+     */
+    public function registration(Request $resquest, ObjectManager $manager, UserPasswordEncoderInterface $encoder){
+        $user = new User();
+        $form = $this->createForm(RegistrationType::class, $user); // liaision des champs de user aux champs du formulaire
+        $form->handleRequest($resquest);
+        if($form->isSubmitted() && $form->isValid()){
+            $hash = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($hash);
+            $manager->persist($user);
+            $manager->flush();
+            
+            return $this->redirectToRoute('security_login');
+        }
+        return $this->render('security/registration.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/connexion", name="security_login")
+     */
+    public function login(){
+        return $this->render('security/login.html.twig');
+    }
+
+    /**
+     * @Route("/deconnexion", name="security_logout")
+     */
+    public function  logout(){}
+
+}
